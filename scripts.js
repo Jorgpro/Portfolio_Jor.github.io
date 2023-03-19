7354eb7cb036f112ecb940b53488c13aaa6df9ba
Jorgpro <115546930+Jorgpro@users.noreply.github.com>
2023-03-19 00:24:08 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let menuVisible = false; 
+//funcion oculta o muestra menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible= false; 
+    }else{
+        document.getElementById("nav").classList= "responsive";
+        menuVisible= true; 
+    }
+}
+function seleccionar(){
+    document.getElementById("nav").classList= "";
+    menuVisible= false; 
+}
+
+
